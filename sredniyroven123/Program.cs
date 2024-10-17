b080361973fce1291434ae99fe8e9c0baaafad9a
Admin <Admin@DESKTOP-UGP0C2G>
2024-10-17 22:00:37 +0200

    Добавьте файлы проекта.

---
+++ sredniyroven123/Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    int n = int.Parse(Console.ReadLine());
+    bool isTrue = (n % 3 != 0) && (n % 7 == 0) && (n % 10 == 0);
+    Console.WriteLine($"The number {n} meets the conditions: {isTrue}");
+}
+catch
+{
+
+}
